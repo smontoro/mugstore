e1732fb7328fdf43c6836bcbe105048c95b239b8
smontoro <sarahjmontoro@gmail.com>
2017-10-30 21:29:57 -0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,43 @@
+
+function Mugs(image, name, price) {
+	this.image = image
+	this.name = name
+	this.price = price
+}
+
+var bigMugs = new Mugs('img/bigMugs.png', 'I Like Big Mugs', '$5.00')
+var meYou = new Mugs('img/meyou.png', 'You, Me', '$6.75')
+
+var mugs = [bigMugs, meYou]
+
+
+	for (var i = 0; i < mugs.length; i++) {
+		var mainDiv = document.getElementById('mainDiv')
+		var rowDiv = document.createElement('div')
+		rowDiv.className = 'row'
+		var colDiv = document.createElement('div')
+		colDiv.className = 'my-4 col-4'
+		var cardDiv = document.createElement('div')
+		cardDiv.className = 'card'
+		var newImg = document.createElement('img')
+		newImg.className = 'image-size'
+		var newH4 = document.createElement('h4')
+		var newP = document.createElement('p')
+		var newButton = document.createElement('button')
+		newButton.textContent="Buy"
+		newButton.className = 'btn'
+
+		newImg.src = mugs[i].image
+		newH4 = document.createTextNode(mugs[i].name)
+		newP = document.createTextNode(mugs[i].price)
+
+		cardDiv.appendChild(newImg)
+		cardDiv.appendChild(newH4)
+		cardDiv.appendChild(newP)
+		cardDiv.appendChild(newButton)
+		colDiv.appendChild(cardDiv)
+		rowDiv.appendChild(colDiv)
+		document.getElementById('mainDiv').appendChild(rowDiv)
+
+	}	
+
