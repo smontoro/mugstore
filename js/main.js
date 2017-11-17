8005a5607a31f479061d39467e102db91c7962d6
smontoro <sarahjmontoro@gmail.com>
2017-11-17 11:09:19 -0800

    added new mugs

--- js/main.js
+++ js/main.js
@@ -7,6 +7,11 @@ function Mugs(image, name, price) {
 
 var bigMugs = new Mugs('img/bigMugs.png', 'I Like Big Mugs', '$5.00')
 var meYou = new Mugs('img/meyou.png', 'You, Me', '$6.75')
+var anger = new Mugs('img/anger.jpg', 'Anger Mangement Mug', '$8.00')
+var eighty = new Mugs('img/eighty.jpeg', 'Turning 80', '$7.00')
+var nobodycares = new Mugs('img/nobodycares.jpg', 'Nobody Cares', '$9.00')
+var omg = new Mugs('img/omg.jpg', 'OMG', '$5.99')
+
 
 var mugs = [bigMugs, meYou]
 
@@ -25,7 +30,7 @@ var mugs = [bigMugs, meYou]
 		var newP = document.createElement('p')
 		var newButton = document.createElement('button')
 		newButton.textContent="Buy"
-		newButton.className = 'btn'
+		newButton.className = 'btn btn-info'
 
 		newImg.src = mugs[i].image
 		newH4 = document.createTextNode(mugs[i].name)
