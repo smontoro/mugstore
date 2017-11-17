00dfbe81cb359c47f27f78dbbc22b03cecda20b3
smontoro <sarahjmontoro@gmail.com>
2017-11-17 14:43:28 -0800

    beautified

--- js/main.js
+++ js/main.js
@@ -1,8 +1,7 @@
-
 function Mugs(image, name, price) {
-	this.image = image
-	this.name = name
-	this.price = price
+    this.image = image
+    this.name = name
+    this.price = price
 }
 
 var bigMugs = new Mugs('img/bigMugs.png', 'I Like Big Mugs', '$5.00')
@@ -16,50 +15,31 @@ var omg = new Mugs('img/omg.jpg', 'OMG', '$5.99')
 var mugs = [bigMugs, meYou, anger, eighty, nobodycares, omg]
 
 
-	for (var i = 0; i < mugs.length; i++) {
-		var cardDiv = document.createElement('div')
-			cardDiv.className = "card text-center my-2 col-3 mx-2 p-2"
-			cardDiv.id = 'card' +[i]
-		var newImg = document.createElement('img')
-			newImg.className = "card-img-top"
-			newImg.src = mugs[i].image
-		var cardBodyDiv = document.createElement("div")
-			cardBodyDiv.className = "card-body"
-		var cardTitle = document.createElement("h4")
-			cardTitle.className = "card-title text-center"
-		var	cardTitleText = document.createTextNode(mugs[i].name)
-		var cardText = document.createElement("p")
-			cardText.className = "card-text text-center"
-			cardText = document.createTextNode(mugs[i].price)
-		var cardButton = document.createElement("button")
-			cardButton.className = "btn btn-info"
-			cardButton.textContent = "Buy"
+for (var i = 0; i < mugs.length; i++) {
+    var cardDiv = document.createElement('div')
+    cardDiv.className = "card text-center my-2 col-3 mx-2 p-2"
+    cardDiv.id = 'card' + [i]
+    var newImg = document.createElement('img')
+    newImg.className = "card-img-top"
+    newImg.src = mugs[i].image
+    var cardBodyDiv = document.createElement("div")
+    cardBodyDiv.className = "card-body"
+    var cardTitle = document.createElement("h4")
+    cardTitle.className = "card-title text-center"
+    var cardTitleText = document.createTextNode(mugs[i].name)
+    var cardText = document.createElement("p")
+    cardText.className = "card-text text-center"
+    cardText = document.createTextNode(mugs[i].price)
+    var cardButton = document.createElement("button")
+    cardButton.className = "btn btn-info"
+    cardButton.textContent = "Buy"
 
-			cardDiv.appendChild(newImg)
-			cardDiv.appendChild(cardBodyDiv)
-			cardTitle.appendChild(cardTitleText)
-			cardBodyDiv.appendChild(cardTitle)
-			cardBodyDiv.appendChild(cardText)
-			cardDiv.appendChild(cardButton)
-			document.getElementById('rowDiv').appendChild(cardDiv)
+    cardDiv.appendChild(newImg)
+    cardDiv.appendChild(cardBodyDiv)
+    cardTitle.appendChild(cardTitleText)
+    cardBodyDiv.appendChild(cardTitle)
+    cardBodyDiv.appendChild(cardText)
+    cardDiv.appendChild(cardButton)
+    document.getElementById('rowDiv').appendChild(cardDiv)
 
-	}	
-
-
-//how can I get these mugs to line up??
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+}
